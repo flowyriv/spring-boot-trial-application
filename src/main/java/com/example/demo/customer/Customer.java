90666831b624345cda020cb8d668ab90e8757ef4
flowyriv <river.zebari@gmail.com>
2021-06-02 00:16:03 -0700

    my spring boot entry including a local h2 database interacting with crud operations, exception handlers, and tests. This was all done with spring boot (jdk16), lombok, and maven.

---
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -0,0 +1,55 @@
+package com.example.demo.customer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Customer {
+
+    @Id
+    private Long id;
+
+    @NotBlank(message = "name must not be empty")
+    private String name;
+
+    @NotBlank(message = "password must not be empty")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @NotBlank(message = "email must not be empty")
+    @Email(message = "email is not formatted correctly")
+    private String email;
+
+    @JsonProperty("customer_id")
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -0,0 +1,32 @@
+package com.example.demo.customer;
+
+import com.example.demo.exception.NotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class CustomerService {
+
+
+    private final CustomerRepository customerRepository;
+
+    List<Customer> getCustomers() {
+        log.info("getCustomers was called");
+        return customerRepository.findAll();
+    }
+
+    Customer getCustomer(Long id) {
+        return customerRepository
+                .findById(id)
+                .orElseThrow(() -> {
+                    NotFoundException notFoundException = new NotFoundException("customer with id '" + id + "' not found");
+                    log.error("error in getCustomer {}", id, notFoundException);
+                    return notFoundException;
+                });
+    }
+}
